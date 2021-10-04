f0e911f44d03d491cf1bb29abe6a721d9acece1c
Almudena De La Rosa <almudenaxico@gmail.com>
2021-10-04 08:41:07 +0000

    ok

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let randomSentence = document.getElementById("hello");
   console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  (function(x, y, z) {
+    let domainName = [];
+    for (var i = 0, l = x.length; i < l; i++) {
+      for (var j = 0, k = 0; j < y.length; j++) {
+        domainName.push([pronoun[i], adj[j], noun[k]] + ".com");
+        if (j == y.length - 1 && k < z.length - 1) {
+          k++;
+          j = -1;
+        }
+      }
+    }
+    let sentence = domainName.join("\n").replaceAll(",", "");
+    randomSentence.innerText = sentence;
+  })(pronoun, adj, noun);
 };
